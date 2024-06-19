9eb0f79e8360ea02dffafbb61ed44b367759e795
addleads <addleadstation@gmail.com>
2024-06-19 13:51:20 -0300

    Update jview.py
    up

--- jview.py
+++ jview.py
@@ -141,7 +141,7 @@ def main():
                         telefone_html += f"{phone_number}<br>"
                 
                 card_content = "<div class='content'>" + \
-                    f"<p><strong><h3>Nome:</h3></strong> <h3>{item.get('nome', '')}</h3></p>" + \
+                    f"<h3>{item.get('nome', '')}</h3></p>" + \
                     f"<p><strong><h3>Fantasia:</h3></strong> {item.get('fantasia', '')}</p>" + \
                     f"<p><strong>CNPJ:</strong> {item.get('cnpj', '')}</p>" + \
                     f"<p><strong>Abertura:</strong> {item.get('abertura', '')}</p>" + \
